4f8213e2726c3f139823d6199fbf3e23d1187c17
1014179971@qq.com <1014179971@qq.com>
2023-01-29 15:27:17 +0800

    部分插件

---
+++ Assets/Editor/ExtractFbx/ExtractMaterials.cs
@@ -0,0 +1,229 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+// ---------------------------【一键从FBX导出材质和设置shader】---------------------------
+public class ExtractMaterials : EditorWindow
+{
+    // 当前选择路径
+    public string modelPath = "Assets";
+    public Rect modelRect;
+    // shader
+    int selectShaderIndex = 0;
+    string[] shaderNameLists;
+    // Scale Factor
+    int scaleFactor = 1;
+
+
+    [MenuItem("MHFrameWork/Tools/导出材质和修改Shader")]
+    public static void showWindow()
+    {
+        EditorWindow.CreateInstance<ExtractMaterials>().Show();
+    }
+
+    private void Awake()
+    {
+        // 获取所有Shader
+        ShaderInfo[] shaderLists = ShaderUtil.GetAllShaderInfo();
+        shaderNameLists = new string[shaderLists.Length];
+        for (int i = 0; i < shaderLists.Length; i++)
+        {
+            shaderNameLists[i] = shaderLists[i].name;
+            if (shaderNameLists[i] == "LayaAir3D/Mesh/PBR(Standard)")
+            {
+                selectShaderIndex = i;
+            }
+        }
+    }
+
+    public void OnGUI()
+    {
+        // 模型路径
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("模型路径 (鼠标拖拽文件夹到这里)");
+        EditorGUILayout.Space();
+        GUI.SetNextControlName("input1");//设置下一个控件的名字
+        modelRect = EditorGUILayout.GetControlRect();
+        modelPath = EditorGUI.TextField(modelRect, modelPath);
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        // Shader下拉列表
+        EditorGUILayout.LabelField("当前选中Shader：");
+        EditorGUILayout.Space();
+        selectShaderIndex = EditorGUILayout.Popup(selectShaderIndex, shaderNameLists);
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        // scale Factor
+        EditorGUILayout.LabelField("模型缩放设置");
+        scaleFactor = EditorGUILayout.IntField("Scale Factor：", scaleFactor);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        // 文件拖拽
+        DragFolder();
+
+        // 设置模型缩放比例
+        if (GUILayout.Button("设置模型缩放比例"))
+        {
+            ForEachModels(true);
+        }
+        EditorGUILayout.Space();
+
+        // 导出材质
+        if (GUILayout.Button("导出材质球"))
+        {
+            ForEachModels(false);
+        }
+
+        EditorGUILayout.Space();
+
+        // 设置Shader
+        if (GUILayout.Button("设置Shader"))
+        {
+            ForEachMaterials();
+        }
+
+
+    }
+
+    // 鼠标拖拽文件
+    void DragFolder()
+    {
+        //鼠标位于当前窗口
+        if (mouseOverWindow == this)
+        {
+            //拖入窗口未松开鼠标
+            if (Event.current.type == EventType.DragUpdated)
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Generic;//改变鼠标外观
+                // 判断区域
+                if (modelRect.Contains(Event.current.mousePosition))
+                    GUI.FocusControl("input1");
+            }
+            //拖入窗口并松开鼠标
+            else if (Event.current.type == EventType.DragExited)
+            {
+                string dragPath = string.Join("", DragAndDrop.paths);
+                // 判断区域
+                if (modelRect.Contains(Event.current.mousePosition))
+                    this.modelPath = dragPath;
+
+                // 取消焦点(不然GUI不会刷新)
+                GUI.FocusControl(null);
+            }
+        }
+    }
+
+    // 遍历模型
+    void ForEachModels(bool isSetModelSetup)
+    {
+        string[] allPath = AssetDatabase.FindAssets("t:GameObject", new string[] { modelPath });
+        // Debug.Log("-- allPath: " + allPath.Length);
+        for (int i = 0, len = allPath.Length; i < len; i++)
+        {
+            string filePath = AssetDatabase.GUIDToAssetPath(allPath[i]);
+            // 设置模型
+            if (isSetModelSetup)
+                setModelSetup(filePath);
+            else
+                ExtractMaterialsFromFBX(filePath);
+        }
+        // 如果选取的是FBX模型文件
+        if (allPath.Length == 0)
+        {
+            if (Path.GetExtension(modelPath) == ".fbx")
+            {
+                // 设置模型
+                if (isSetModelSetup)
+                    setModelSetup(modelPath);
+                else
+                    ExtractMaterialsFromFBX(modelPath);
+            }
+            else
+            {
+                Debug.LogError("当前选择目录未找到FBX文件: " + this.modelPath);
+            }
+        }
+
+    }
+
+    // 从fbx模型导出材质球
+    public void ExtractMaterialsFromFBX(string assetPath)
+    {
+        // 材质目录
+        string materialFolder = Path.GetDirectoryName(assetPath) + "/Material";
+        // 如果不存在该文件夹则创建一个新的
+        if (!AssetDatabase.IsValidFolder(materialFolder))
+            AssetDatabase.CreateFolder(Path.GetDirectoryName(assetPath), "Material");
+        // 获取 assetPath 下所有资源。
+        Object[] assets = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+        foreach (Object item in assets)
+        {
+            if (item.GetType() == typeof(Material))
+            {
+                string path = System.IO.Path.Combine(materialFolder, item.name) + ".mat";
+                // 为资源创建一个新的唯一路径。
+                path = AssetDatabase.GenerateUniqueAssetPath(path);
+                // 通过在导入资源（例如，FBX 文件）中提取外部资源，在对象（例如，材质）中创建此资源。
+                string value = AssetDatabase.ExtractAsset(item, path);
+                // 成功提取( 如果 Unity 已成功提取资源，则返回一个空字符串)
+                if (string.IsNullOrEmpty(value))
+                {
+                    AssetDatabase.WriteImportSettingsIfDirty(assetPath);
+                    AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate);
+                }
+            }
+        }
+        Debug.Log(Path.GetFileName(assetPath) + " 的 Material 导出成功!!");
+    }
+
+    // 修改模型设置
+    void setModelSetup(string path)
+    {
+        ModelImporter importer = (ModelImporter)ModelImporter.GetAtPath(path);
+        if (importer)
+        {
+            importer.globalScale = scaleFactor;
+            importer.SaveAndReimport();
+            Debug.Log("Scale Factor 设置成功: " + Path.GetFileName(path));
+        }
+    }
+
+    // 遍历材质
+    void ForEachMaterials()
+    {
+        string[] allPath = AssetDatabase.FindAssets("t:Material", new string[] { modelPath });
+        if (allPath.Length == 0)
+        {
+            if (System.IO.Path.GetExtension(modelPath) == ".mat")
+                setMaterialShader(modelPath);
+            else
+                Debug.LogError("当前目录未找到Material: " + modelPath);
+            return;
+        }
+
+        for (int i = 0, len = allPath.Length; i < len; i++)
+        {
+            string filePath = AssetDatabase.GUIDToAssetPath(allPath[i]);
+            setMaterialShader(filePath);
+        }
+        Debug.Log("Material Shader 设置完成, 一共: " + allPath.Length + "个");
+    }
+
+    void setMaterialShader(string path)
+    {
+        Material mat = AssetDatabase.LoadAssetAtPath<Material>(path);
+        Shader mShader = Shader.Find(shaderNameLists[selectShaderIndex]);
+        if (mShader == null)
+        {
+            Debug.LogError("设置shader失败, 未找到该shader: " + shaderNameLists[selectShaderIndex]);
+            return;
+        }
+        mat.shader = mShader;
+        // AssetDatabase.Refresh();
+    }
+
+}
---
+++ Assets/xcodeConfig/Editor/IosBuildWindow.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace MHEditor.Build
+{
+    public class IosBuildWindow : EditorWindow
+    {
+        private static IosBuildWindow instance;
+        private static Vector2 scrollPos;
+        private enum Channel
+        {
+            NONE,
+            Ohayoo
+        }
+        private static Channel channel;
+        private static ES3File es3File;
+        private static IosData iosData;
+        private static string configPath;
+        private static bool isReplace;
+
+        [MenuItem("MHFrameWork/Ios Build/Replace", false, 1)]
+        static void ShowWindow()
+        {
+            isReplace = true;
+            LoadData();
+            instance = EditorWindow.GetWindow<IosBuildWindow>();
+            scrollPos = new Vector2(instance.position.x, instance.position.y + 75);
+            instance.Show();
+            
+        }
+        [MenuItem("MHFrameWork/Ios Build/Append", false, 1)]
+        static void SetConfig()
+        {
+            isReplace = false;
+            LoadData();
+            instance = EditorWindow.GetWindow<IosBuildWindow>();
+            scrollPos = new Vector2(instance.position.x, instance.position.y + 75);
+            instance.Show();
+            return;
+            string path = EditorUtility.OpenFilePanel("Choice Config", System.Environment.CurrentDirectory, "json");
+            Debug.Log($"config path=={path}");
+            if (!string.IsNullOrEmpty(path))
+                ConfigPath = path;
+        }
+        private static void LoadData()
+        {
+            es3File = new ES3File("iosBuild.es3");
+            if (es3File.KeyExists(channel.ToString()))
+                iosData = es3File.Load<IosData>(channel.ToString());
+            else
+                iosData = new IosData();
+
+
+        }
+        public static string ConfigPath {
+            get {
+                return "iOSAdd/" + channel.ToString() + "/XCodeConfig.json";
+                
+            }
+            set { configPath = value; }
+        }
+        private void OnGUI()
+        {
+            
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Channel", GUILayout.Width(150));
+            Channel ch = (Channel)EditorGUILayout.EnumPopup(channel);
+            if (channel != ch)
+            {
+                channel = ch;
+                LoadData();
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Version*", GUILayout.Width(150));
+            if(iosData !=null)
+                iosData.bundleVersion = EditorGUILayout.TextField("", iosData.bundleVersion);
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Build Number", GUILayout.Width(150));
+            if (iosData != null)
+            {
+                string buildNumber = EditorGUILayout.TextField("", iosData.buildNumber);
+                iosData.buildNumber = buildNumber;
+            }
+                
+            GUILayout.EndHorizontal();
+            if (!isReplace)
+            {
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("UnityXcode Path", GUILayout.Width(150)))
+                {
+                    if (string.IsNullOrEmpty(iosData.iosPath))
+                        iosData.iosPath = EditorUtility.SaveFilePanel("Build Ios", System.Environment.CurrentDirectory, "", "");
+                    else
+                    {
+                        if (Directory.Exists(iosData.iosPath))
+                        {
+                            DirectoryInfo directoryInfo = new DirectoryInfo(iosData.iosPath);
+                            iosData.iosPath = EditorUtility.SaveFilePanel("Build Ios", directoryInfo.Parent.FullName, directoryInfo.Name, "");
+                        }
+                        else
+                            iosData.iosPath = EditorUtility.SaveFilePanel("Build Ios", iosData.iosPath, "", "");
+                    }
+                }
+                if(iosData != null)
+                    EditorGUILayout.TextField("",iosData.iosPath);
+
+                GUILayout.EndHorizontal();
+            }
+            
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Export Project"))
+            {
+                if (isReplace)
+                    ExportProject();
+                else
+                    ExportAppendProject();
+            }
+            GUILayout.EndHorizontal();
+        }
+        static void Build()
+        {
+            PlayerSettings.bundleVersion = iosData.bundleVersion;
+            PlayerSettings.iOS.buildNumber = iosData.buildNumber;
+        }
+
+        private static void ExportProject()
+        {
+            Build();
+            
+            string path = iosData.iosPath;
+            if (string.IsNullOrEmpty(iosData.iosPath))
+                path = EditorUtility.SaveFilePanel("Build Ios", System.Environment.CurrentDirectory, "", "");
+            else
+            {
+                if (Directory.Exists(iosData.iosPath))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(iosData.iosPath);
+                    path = EditorUtility.SaveFilePanel("Build Ios", directoryInfo.Parent.FullName, directoryInfo.Name, "");
+                }
+                else
+                    path = EditorUtility.SaveFilePanel("Build Ios", iosData.iosPath, "", "");
+            }
+            if (!string.IsNullOrEmpty(path))
+            {
+                iosData.iosPath = path;
+                SaveData();
+                if (EditorUserBuildSettings.activeBuildTarget != BuildTarget.iOS)
+                    EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.Android);
+                //var options = BuildOptions.AcceptExternalModificationsToPlayer;//指定目录下必须有xcode工程
+                var options = BuildOptions.AllowDebugging;
+                BuildPipeline.BuildPlayer(GetBuildScenes(), iosData.iosPath, BuildTarget.iOS, options);
+                EditorUtility.RevealInFinder(iosData.iosPath);
+            }
+            
+        }
+        private static void ExportAppendProject()
+        {
+            Build();
+
+            string path = iosData.iosPath;
+            if (!string.IsNullOrEmpty(path))
+            {
+                iosData.iosPath = path;
+                SaveData();
+                if (EditorUserBuildSettings.activeBuildTarget != BuildTarget.iOS)
+                    EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.Android);
+                string projPath = UnityEditor.iOS.Xcode.PBXProject.GetPBXProjectPath(path);
+                
+                var options = BuildOptions.AllowDebugging;
+                if(File.Exists(projPath))
+                    options = BuildOptions.AcceptExternalModificationsToPlayer;//指定目录下必须有xcode工程
+                
+                BuildPipeline.BuildPlayer(GetBuildScenes(), iosData.iosPath, BuildTarget.iOS, options);
+                EditorUtility.RevealInFinder(iosData.iosPath);
+            }
+            else
+                Debug.LogError("到处路径为空");
+
+        }
+        private static string[] GetBuildScenes()
+        {
+            List<string> names = new List<string>();
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                if (!EditorBuildSettings.scenes[i].enabled) continue;
+                names.Add(EditorBuildSettings.scenes[i].path);
+
+            }
+            return names.ToArray();
+        }
+        private static void SaveData()
+        {
+            es3File.Save<IosData>(channel.ToString(), iosData);
+            es3File.Sync();
+        }
+
+
+        /*********************/
+        public class IosData
+        {
+            public string bundleVersion;
+            public string buildNumber;
+            public string iosPath;
+        }
+    }
+}
+
+
